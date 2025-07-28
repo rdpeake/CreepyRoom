651639c79d71edfb0e40273cf2b1027b051af983
rdpeake <rdpeake@adam.com.au>
2025-07-28 18:52:00 +0930

    Add project files.

---
+++ CreepyRoom/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace CreepyRoom
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
